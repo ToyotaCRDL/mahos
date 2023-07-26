62c5621e7e66bec1918029df0cd690d082c99958
Kosuke Tahara <ksk.tahara@gmail.com>
2023-07-26 22:35:09 +0900

    bump version to 0.2.1

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Toyota Central R&D Labs., Inc"
 author = "Kosuke Tahara"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.2.1"
 
 
 # -- General configuration ---------------------------------------------------
